5d16dcd6a7aa199b9a84ad4d8c2662e28790136d
Dennis Smuda <smuda.dennis@gmail.com>
2026-02-14 17:58:14 +0100

    chore: init vite/vue/css-doodle project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// import { ref } from 'vue';
+import Test from './Test.vue';
+</script>
+
+<template>
+  <css-doodle grid="5" id="doodle">
+
+    <Test />
+  </css-doodle>
+</template>
+
+<style scoped>
+#doodle {
+  background: var(--color-near-black);
+}
+</style>
