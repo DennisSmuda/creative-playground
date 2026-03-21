93bcf2207eda93b22a7fb050974e728ddf06676d
Dennis Smuda <smuda.dennis@gmail.com>
2026-03-21 20:19:24 +0100

    feat: add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    template: {
-      compilerOptions: {
-        isCustomElement: tag => tag.startsWith('css-doodle'),
+  plugins: [
+    tailwindcss(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => tag.startsWith('css-doodle'),
+        },
       },
-    },
-  })],
+    }),
+  ],
 })
